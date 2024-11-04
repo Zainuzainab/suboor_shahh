07de6eeebf6fbab1e819653ac1fe6327dd555d90
Zainuzainab <147390714+Zainuzainab@users.noreply.github.com>
2024-11-04 12:51:22 +0530

    Add files via upload

---
+++ s2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main(){
+    int a= 10;
+    printf("%d",a);
+    float b=10.3223;
+    printf("% .2f",b);
+    char c='a';
+    printf("%c",c);
+    char s[]="hello";
+    printf("%s",s);
+
+    }

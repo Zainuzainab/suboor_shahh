3d2322f8847282fd7a25dc3fa2834e1bd72df20d
Zainuzainab <147390714+Zainuzainab@users.noreply.github.com>
2024-11-04 12:54:29 +0530

    Add files via upload

---
+++ arthm_opt.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main(){
+    int a=10;
+    int b=29;
+    int sum=a+b;
+    int dif=a-b;
+    int mul=a*b;
+    int div=a/b;
+    int mod=a%b;
+    printf("%d\n",sum);
+    printf("%d\n",dif);
+    printf("%d\n",mul);
+    printf("%d\n",div);
+    printf("%d\n",mod);
+   
+}
